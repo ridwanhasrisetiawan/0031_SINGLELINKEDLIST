50dc9f3bf9af60f867b4281ba6d6759829b52855
ridwanhasrisetiawan <ridwanhasrisetiawan@gmail.com>
2025-05-08 19:58:58 +0700

    COMMIT5

--- singlelinkedlist.cpp
+++ singlelinkedlist.cpp
@@ -57,5 +57,25 @@ class LinkedList
         nodeBaru->next = current;
         previous->next = nodeBaru;
     }
-    
+
+    bool listEmpty()
+    {
+        return (START == NULL);
+    }
+
+    bool Search(int nim, Node **previous, Node **current)
+    {
+        *previous = START;
+        *current = START;
+
+        while ((*current != NULL) && (nim != (*current)->noMhs))
+        {
+            *previous = *current;
+            *current = (*current)->next;
+        }
+
+        return (*current != NULL);
+    }
+
+    bool delNode(int nim)
 }
