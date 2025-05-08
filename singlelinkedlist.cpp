a34c90455147a927645bb82d51f55b895b2f2e40
ridwanhasrisetiawan <ridwanhasrisetiawan@gmail.com>
2025-05-08 20:10:30 +0700

    COOMIT6

--- singlelinkedlist.cpp
+++ singlelinkedlist.cpp
@@ -13,7 +13,7 @@ class LinkedList
 {
     Node *START;
 
-    public:
+public:
     LinkedList()
     {
         START = NULL;
@@ -34,7 +34,7 @@ class LinkedList
             {
                 cout << "\nDuplikasi noMhs tidak diijinkan\n";
                 return;
-            } 
+            }
             nodeBaru->next = START;
             START = nodeBaru;
             return;
@@ -78,4 +78,36 @@ class LinkedList
     }
 
     bool delNode(int nim)
-}
+    {
+        Node *current, *previous;
+        if (!Search(nim, &previous, &current))
+            return false;
+
+        if (current == START)
+            START = START->next;
+        else
+            previous->next = current->next;
+
+        delete current;
+        return true;
+    }
+
+    void traverse()
+    {
+        if (listEmpty())
+        {
+            cout << "\nList Kosong\n";
+        }
+        else
+        {
+            cout << "\nData didalam list adalah:\n";
+            Node *currentNode = START;
+            while (currentNode != NULL)
+            {
+                cout << currentNode->noMhs << endl;
+                currentNode = currentNode->next;
+            }
+            cout << endl;
+        }
+    }
+};
