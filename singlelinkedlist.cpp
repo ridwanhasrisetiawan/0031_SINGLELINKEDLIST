d642d804945d9a407fb5aadf837a346d058250c4
ridwanhasrisetiawan <ridwanhasrisetiawan@gmail.com>
2025-05-08 20:18:49 +0700

    COMMIT7

--- singlelinkedlist.cpp
+++ singlelinkedlist.cpp
@@ -111,3 +111,30 @@ public:
         }
     }
 };
+
+int main()
+{
+    LinkedList mhs;
+    int nim;
+    char ch;
+
+    do
+    {
+        cout << "Menu" << endl;
+        cout << "1. Menambah data kedalam list" << endl;
+        cout << "2. Menghapus data dari dalam list" << endl;
+        cout << "3. Menampilkan semua  data didalam list" << endl;
+        cout << "4. Mencari data dalam lsit" << endl;
+        cout << "5. Keluar" << endl;
+        cout << endl
+             << "Masukkan pilihan (1-5): ";
+        cin >> ch;
+        switch (ch)
+        {
+            case '1':
+            {
+                
+            }
+        }
+    }
+}
