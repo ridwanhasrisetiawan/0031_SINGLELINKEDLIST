456cd7b1315e4bb62f60cb0143d20fba6d3bff20
ridwanhasrisetiawan <ridwanhasrisetiawan@gmail.com>
2025-05-08 20:42:53 +0700

    COMMIT10

--- singlelinkedlist.cpp
+++ singlelinkedlist.cpp
@@ -177,7 +177,23 @@ int main()
                 cout << endl
                      << "Data tidak ditemukan" << endl;
             else
+            {
+                cout << endl
+                << "Data ditemukan" << endl;
+                cout << "\nNo Mahasiswa: " << current->noMhs << endl;
+                cout << "\n";
+            }
         }
+        break;
+        case '5':
+        {
         }
-    }
+        break;
+        default:
+        {
+            cout << "Pilihan salah !." << endl;
+        }
+        break;
+        }
+    } while (ch != '5');
 }
