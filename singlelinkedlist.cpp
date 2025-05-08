2496164c8421dcb46e86aeb2e27c1daaf5ca2cc1
ridwanhasrisetiawan <ridwanhasrisetiawan@gmail.com>
2025-05-08 19:32:45 +0700

    COMMIT2

---
+++ singlelinkedlist.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+class Node
+{
+public:
+    int noMhs;
+    Node *next;
+};
+
+class LinkedList
+{
+    Node *START;
+
+    public:
+    LinkedList()
+    {
+        START = NULL;
+    }
+}
